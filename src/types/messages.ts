a138b6ff11662746f87eb0e45f00cec75cf6ac46
Luke Elabor <lukeybooi@gmail.com>
2025-05-28 23:29:00 +0200

    feat: created messages interface

---
+++ src/types/messages.ts
@@ -0,0 +1,16 @@
+export interface Message {
+  id: string;
+  deviceType: 'desktop' | 'mobile';
+  message: string;
+  timestamp: number;
+}
+
+export type MessageListProps = {
+  messages: Message[];
+  currentDevice: 'desktop' | 'mobile';
+};
+
+export type MessageInputProps = {
+  onSend: (deviceType: 'desktop' | 'mobile', message: string) => void;
+  deviceType: 'desktop' | 'mobile';
+};
